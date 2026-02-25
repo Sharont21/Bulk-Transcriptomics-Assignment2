3644699f2569bba1af3e7280f7c50b6bb2a0021e
Sharon Tsukernik <sharont@l5.nibi.sharcnet>
2026-02-24 20:16:36 -0500

    Adding salmong quantification script

---
+++ scripts/run_salmon_quant.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+#SBATCH --job-name=salmon_quant
+#SBATCH --time=02:00:00
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=8G
+#SBATCH --output=slurm-salmon-quant-%j.out
+#SBATCH --error=slurm-salmon-quant-%j.err
+
+module load StdEnv/2023
+module load salmon
+
+FASTQ_DIR=$HOME/Bulk-Transcriptomics-Assignment2/data/fastq
+INDEX_DIR=$HOME/Bulk-Transcriptomics-Assignment2/data/reference/salmon_index
+OUT_DIR=$HOME/Bulk-Transcriptomics-Assignment2/data/salmon
+
+# List of samples
+SRR_LIST=(
+  SRR10551657
+  SRR10551658
+  SRR10551659
+  SRR10551660
+  SRR10551661
+  SRR10551662
+  SRR10551663
+  SRR10551664
+  SRR10551665
+)
+
+for SRR in "${SRR_LIST[@]}"; do
+  echo "Quantifying $SRR"
+  salmon quant \
+    -i $INDEX_DIR \
+    -l A \
+    -r $FASTQ_DIR/${SRR}_1.fastq \
+    -p 4 \
+    --validateMappings \
+    -o $OUT_DIR/${SRR}_quant
+done
